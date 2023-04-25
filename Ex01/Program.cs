962fc25259d80c1fa618eeea1b93ccfa3dc82ce8
Jun Shimura <junshimura@gmail.com>
2023-04-25 17:24:30 +0900

    ex01

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -8,8 +8,10 @@
             var height = double.Parse(Console.ReadLine()); //身長
             Console.Write("体重の入力(kg):");
             var weight = double.Parse(Console.ReadLine()); ; // 体重
-            var bmi = weight / ((height / 100) * (height / 100)); // BMIを求める数式
-            Console.Write($"身長={height},体重={weight},\nBMI={bmi}");
+            //var bmi = weight / ((height / 100) * (height / 100)); // BMIを求める数式
+            height /= 100;
+            var bmi = weight / (height * height);  // BMIを算出
+            Console.Write($"身長={height},体重={weight},\nBMI={bmi.ToString("F2")}");
         }
     }
 }
