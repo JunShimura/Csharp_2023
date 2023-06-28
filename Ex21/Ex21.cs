f7faf09c60f53f0e4a42c8030106ad20a9be3c1a
Jun Shimura <junshimura@gmail.com>
2023-06-28 14:28:46 +0900

    ex21

---
+++ Ex21/Ex21.cs
@@ -0,0 +1,39 @@
+﻿namespace Ex21
+{
+    class Ex21
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("名前：");
+            string s1 = Console.ReadLine();
+            int no;
+            while (true)
+            {
+                Console.WriteLine("ほめ指数を1～5で入力：");
+                if (int.TryParse(Console.ReadLine(), out no))
+                {
+                    if (no >= 1 && no <= 5)
+                    {
+                        break;
+                    }
+
+                }
+                Console.WriteLine("入力エラー");
+            }
+
+            Console.WriteLine(Praise(s1, no));
+        }
+        static string Praise(string s, int praiseIndex)
+        {
+            //ここに処理を追加
+            string answer = string.Empty;
+            string[] message = { "かっこいい", "ゲームがうまい", "つよい", "もうどうにかして", "つきあってください" };
+            for (var i = 1; (i <= praiseIndex) && praiseIndex <= message.Length; i++)
+            {
+                answer += $"{s}さん{message[i - 1]}!\n";
+            }
+            return answer;
+        }
+
+    }
+}
---
+++ Ex21_ex2/Ex21_ex2.cs
@@ -0,0 +1,61 @@
+﻿namespace Ex21_ex2
+{
+    internal class Ex21_ex2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                Praise(
+                    InputString("名前："),
+                    InputInt("ほめ指数", 1, 6)
+                    )
+                );
+        }
+        static string Praise(string s, int praiseIndex)
+        {
+            //ここに処理を追加
+            string answer = string.Empty;
+            string[] message = { "かっこいい", "ゲームがうまい", "つよい", "もうどうにかして", "つきあってください" };
+            for (var i = 1; i <= praiseIndex && praiseIndex <= message.Length; i++)
+            {
+                answer += $"{s}さん{message[i - 1]}!\n";
+            }
+            return answer;
+        }
+
+        // MethodSampleから移植 https://github.com/JunShimura/Csharp_2023/blob/master/MethodSample/MethodSample.cs
+        private static string InputString(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        private static int InputInt(string message)
+        {
+            int i;
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out i))
+                {
+                    break;
+                }
+                Console.WriteLine("入力エラー");
+            }
+            return i;
+        }
+        private static int InputInt(string message, int min, int max)
+        {
+            int i;
+            while (true)
+            {
+                i = InputInt($"{message} 値の範囲は{min}以上・{max}未満");
+                if (i >= min && i < max)
+                {
+                    break;
+                }
+                Console.WriteLine("入力範囲エラー");
+            }
+            return i;
+        }
+    }
+}
