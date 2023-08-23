874489ee249ed4e20536f8b6140328ee4a9fe4e6
Jun Shimura <junshimura@gmail.com>
2023-08-23 17:32:43 +0900

    ex27_hint

--- Ex27_hint/Ex27_hint.cs
+++ Ex27_hint/Ex27_hint.cs
@@ -7,23 +7,21 @@ namespace Ex27_hint
         static void Main(string[] args)
         {
             Rectangle rectangle = new Rectangle(
-                (float)InputUtility.InputNumber("幅："),
-                (float)InputUtility.InputNumber("高さ：")
+                (float)InputUtility.InputNumber("長方形の幅："),
+                (float)InputUtility.InputNumber("長方形の高さ：")
             );
-            Rectangle rectangle1 = new Rectangle(3, 2.5f);
             Console.WriteLine($"rectangleの面積は{rectangle.GetSurface()}、周囲の長さは{rectangle.GetPerimeter()}");
-            Console.WriteLine($"rectangle1の面積は{rectangle1.GetSurface()}、周囲の長さは{rectangle1.GetPerimeter()}");
+
+            Circle circle = new Circle(
+                (float)InputUtility.InputNumber("円の半径：")
+                );
 
             Box box = new Box(
                 (float)InputUtility.InputNumber("幅："),
                 (float)InputUtility.InputNumber("高さ："),
                 (float)InputUtility.InputNumber("奥行：")
                );
-
-            Box box1 = new Box(3, 4.5f, 7);
-            // 作られたboxのインスタンスを用いて表面積と体積を取り出して表示
             Console.WriteLine($"boxの表面積は{box.GetSurface()}、体積は{box.GetVolume()}");
-            Console.WriteLine($"box1の表面積は{box1.GetSurface()}、体積は{box1.GetVolume()}");
         }
     }
     // 平面の図形
@@ -71,6 +69,14 @@ namespace Ex27_hint
             return (width + height) * 2;
         }
 
+    }
+    class Circle
+    {
+        public float radius;
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
     }
 
     // Boxクラスをこの下に作る
