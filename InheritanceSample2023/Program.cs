aae4999cb3b6732846656cd4defdbbc4b9cae743
Jun Shimura <j.shimura@sugawara.ac.jp>
2023-12-04 15:49:31 +0900

    Birthday

--- InheritanceSample2023/Program.cs
+++ InheritanceSample2023/Program.cs
@@ -6,8 +6,8 @@
         {
             Person[] persons 
             = { 
-                new Person("Jun Shimura", 25), 
-                new Student("Riku Sawamura", 19,6)
+                new Person("Jun Shimura", new DateTime(2001,12,10)), 
+                new Student("Riku Sawamura", new DateTime(2001,12,10),6)
             };
             foreach (Person person in persons)
             {
@@ -21,11 +21,11 @@
     class Person
     {
         private string name;    //名前
-        private int age;        //年齢
-        public Person(string name, int age = 0)
+        private readonly DateTime birthday;  // 生年月日
+        public Person(string name, DateTime birthday)
         {
             this.name = name;
-            this.age = age;
+            this.birthday = birthday;
         }
         /// <summary>
         /// 自己紹介
@@ -40,13 +40,13 @@
         /// <param name="p">自己紹介する人物</param>
         static public void SelfIntroduction(Person p)
         {
-            Console.WriteLine($"私の名前は{p.name}です、年齢は{p.age}です。");
+            Console.WriteLine($"私の名前は{p.name}です、誕生日は{p.birthday}です。");
         }
     }
     class Student : Person
     {
         private int id;  // 学籍番号
-        public Student(string name, int age = 0, int id = 0) : base(name, age)
+        public Student(string name, DateTime birthday, int id = 0) : base(name, birthday)
         {
             this.id = id;
         }
